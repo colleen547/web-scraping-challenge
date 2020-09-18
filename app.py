3d727db0f0a7c8b527cc12bbd2ef703278db4ff2
colleen547 <66704475+colleen547@users.noreply.github.com>
2020-09-18 01:01:57 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+# Dependencies
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
+
+# Create a route that renders index.html template and finds data from mongo
+@app.route("/")
+def home():
+
+    # Find data from mongo database
+    mars = mongo.db.mars.find_one()
+
+    # Return template and data
+    return render_template("index.html", mars=mars)
+
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
+    mars=mongo.db.mars
+    # Run the scrape function
+    mars_data = scrape_mars.scrape_info()
+
+    # Update the Mongo database using update and upsert=True
+    mars.update({}, mars_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(port=5001)
